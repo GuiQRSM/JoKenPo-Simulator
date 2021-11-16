fa88e161e29e11e07facbebf3bc81ce88a167221
GuiQRSM <endbeob15@gmail.com>
2021-11-16 16:32:07 -0300

    Implements inside Scaffold(), the appBar: and body: attributes

--- lib/GameJKP.dart
+++ lib/GameJKP.dart
@@ -11,7 +11,19 @@ class _GamejkpState extends State<Gamejkp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text(
+            "JoKenPo Simulator",
+        style: TextStyle(
+          color: Colors.teal,
+          fontWeight: FontWeight.w700
+        ),),
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: Container(
+        decoration: BoxDecoration(border: Border.all(width: 3, color: Colors.purple)),
+        width: double.infinity,
+      ),
     );
   }
 }
