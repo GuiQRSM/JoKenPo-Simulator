7d6ebdc9523bab541f2d121f8cf8c3a4d8893b94
GuiQRSM <endbeob15@gmail.com>
2021-11-16 16:21:27 -0300

    Implement a stful in the GameJPK file and install it in the home: of MaterialApp.

---
+++ lib/GameJKP.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Gamejkp extends StatefulWidget {
+  const Gamejkp({Key? key}) : super(key: key);
+
+  @override
+  _GamejkpState createState() => _GamejkpState();
+}
+
+class _GamejkpState extends State<Gamejkp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:jokenpo_simulator/GameJKP.dart';
 
 void main() {
 
  runApp(
    MaterialApp(
      debugShowCheckedModeBanner: false,
-     //home: ,
+     home: Gamejkp() ,
    ),
  );
 
