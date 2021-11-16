2d893826d4d004bb48481c240732d01ee990a29c
GuiQRSM <endbeob15@gmail.com>
2021-11-16 16:50:37 -0300

    Pre casts the scope of the interface on Scaffold attributes.

--- lib/GameJKP.dart
+++ lib/GameJKP.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Gamejkp extends StatefulWidget {
@@ -8,6 +9,9 @@ class Gamejkp extends StatefulWidget {
 }
 
 class _GamejkpState extends State<Gamejkp> {
+
+  var _text = "Resultado!";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +24,37 @@ class _GamejkpState extends State<Gamejkp> {
         ),),
         backgroundColor: Colors.tealAccent,
       ),
-      body: Container(
-        decoration: BoxDecoration(border: Border.all(width: 3, color: Colors.purple)),
-        width: double.infinity,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16),
+            child:Text(
+                "O desafiador",
+            style: TextStyle(
+            fontSize: 15,
+            color: Colors.teal,
+            fontWeight: FontWeight.w700,
+             ),
+            ),
+          ),
+          //Image.asset(),
+          Text(
+              "$_text",
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.teal,
+            fontWeight: FontWeight.w700,
+           ),
+          ),
+          Row(
+            children: <Widget>[
+              //Image.asset(),
+              //Image.asset(),
+              //Image.asset(),
+            ],
+          )
+        ],
       ),
     );
   }
